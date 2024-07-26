5bd864c0027be84feb89d33c156cbc9457902200
Editor9948 <olotuoladapoayomide@gmail.com>
2024-07-26 19:52:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const apiKey= 'YOUR_API_KEY';// Get your API key from OpenWeatherMap
+const apiUrl='http://api.openweathermap.org/data/2.5/weather';
+const locationInput= document.getElementById('locationInput');
+const searchButton= document.getElementById('searchButton');
+const locationElement=document.getElementById('location');
+const temperatureElement= document.getElementById('temperature');
+const descriptionElement= document.getElementById('description');
+searchButton.addEventListener('click',()=>{
+    const location= LocationInput.value;
+    if  (location) {fetchweather (location);
+    }
+});
+function fetchWeather (location) {
+    const url = `${apiUrl}? 
+    q=${location}&appid =${apiKey}&units=metric`;
+    fetch (Url). then(response=>response.json()). then (data=>{
+      locationElement.textContent=data.name;
+      temperatureElement.textContent= `${Math.round(data.main.temp)}
+      °C`;
+      descriptionElement.textContent= data.weather[0].description;
+    })
+       .catch (error => {
+           console. error ('Error fecthing weather data:', error);
+       });
+       }
